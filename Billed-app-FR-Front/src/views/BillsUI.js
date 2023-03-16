43066ab494a9d39db1ebaffe649c95f7f0b03283
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-03-16 14:35:29 +0100

    :bug: [BillsUI.js] date sorting bug fixed
    bug fixed by sorting the bills by date when displaying it

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -20,7 +20,10 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  if(data){
+    data.sort((a, b) => (a.date < b.date) ? 1 : -1)
+    return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  }
 }
 
 export default ({ data: bills, loading, error }) => {
