f8fc9d138da349022e60638d7e36e7b831fede8a
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-03-26 17:02:40 +0200

    :white_check_mark: [Bills.js] Add new bill test
    when add new bill button is clicked, the new bill page is displayed

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,11 +2,13 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import { screen, waitFor } from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
+import Bills from '../containers/Bills.js';
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
 
@@ -27,6 +29,7 @@ describe("Given I am connected as an employee", () => {
       const windowIcon = screen.getByTestId('icon-window')
       expect(windowIcon.classList).toContain('active-icon')
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -34,5 +37,47 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+
+    describe("When I click on New Bill button", () => {
+      test("Then it should navigate to New Bill page", async () => {
+
+        // connect as employee
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+        router()
+        window.onNavigate(ROUTES_PATH.Bills)
+
+        // init bills display
+        const billsContainer = new Bills({
+          document,
+          onNavigate,
+          firestore: null,
+          localStorage: window.localStorage,
+        });
+
+        await waitFor(() => screen.getByTestId('btn-new-bill'))
+        const newBillBtn = screen.getByTestId('btn-new-bill')
+
+        const handleClickNewBill = jest.fn(() => billsContainer.handleClickNewBill())
+
+        newBillBtn.addEventListener('click', handleClickNewBill)
+
+        userEvent.click(newBillBtn)
+
+        //the function is called
+        expect(handleClickNewBill).toHaveBeenCalled()
+
+        //new bill page is displayed
+        expect(screen.getByTestId("form-new-bill")).toBeTruthy()
+      })
+    })
+
   })
+
 })
