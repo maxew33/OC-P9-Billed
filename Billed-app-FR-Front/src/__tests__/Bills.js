20c95af9518648472178b85db6ffb3130376e6c8
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-03-28 14:18:53 +0200

    :white_check_mark: [Bills.js] Add click on icon eye test
    when I click on the icon eye, the bill is shown

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -14,32 +14,39 @@ import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
+
     test("Then bill icon in vertical layout should be highlighted", async () => {
 
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
+
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
+
       router()
       window.onNavigate(ROUTES_PATH.Bills)
+
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
+
       expect(windowIcon.classList).toContain('active-icon')
     })
 
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
+
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
+
       expect(dates).toEqual(datesSorted)
     })
 
     describe("When I click on New Bill button", () => {
-      test("Then it should navigate to New Bill page", async () => {
+      test("Then it should navigate to New Bill page", () => {
 
         // connect as employee
         Object.defineProperty(window, 'localStorage', { value: localStorageMock })
@@ -50,6 +57,7 @@ describe("Given I am connected as an employee", () => {
         const root = document.createElement("div")
         root.setAttribute("id", "root")
         document.body.append(root)
+
         router()
         window.onNavigate(ROUTES_PATH.Bills)
 
@@ -57,11 +65,10 @@ describe("Given I am connected as an employee", () => {
         const billsContainer = new Bills({
           document,
           onNavigate,
-          firestore: null,
+          store: null,
           localStorage: window.localStorage,
-        });
+        })
 
-        await waitFor(() => screen.getByTestId('btn-new-bill'))
         const newBillBtn = screen.getByTestId('btn-new-bill')
 
         const handleClickNewBill = jest.fn(() => billsContainer.handleClickNewBill())
@@ -78,6 +85,47 @@ describe("Given I am connected as an employee", () => {
       })
     })
 
+    describe("When I click on Eye icon", () => {
+      test("Then a modal should open", () => {
+
+        // connect as employee
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+
+        // init bills display
+        const billsContainer = new Bills({
+          document,
+          onNavigate,
+          store: null,
+          localStorage: window.localStorage,
+        })
+
+        // DOM creation
+        document.body.innerHTML = BillsUI({ data: bills })
+
+        const iconEyes = screen.getAllByTestId('icon-eye')
+
+        const handleClickIconEye = jest.fn(icon => billsContainer.handleClickIconEye(icon))
+
+        // emulate the .modal
+        $.fn.modal = jest.fn()
+
+        iconEyes.forEach(icon => {
+          icon.addEventListener('click', () => handleClickIconEye(icon))
+
+          userEvent.click(icon)
+
+          //the function is called
+          expect(handleClickIconEye).toHaveBeenCalled()
+
+          //new bill page is displayed
+          expect(screen.getByText('Justificatif')).toBeTruthy()
+        })
+
+      })
+    })
   })
 
 })
